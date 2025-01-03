6ea3106f3392897a130feb9c607ab3199643c0c6
kolli balarama krishna reddy <22501a4428@pvpsit.ac.in>
2025-01-03 16:42:03 +0530

    Update kafka_stream.py

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -1,2 +1,26 @@
 from datetime import datetime
-from airflow import dag
+from airflow import DAG 
+from airflow.operators.python import PythonOperator
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2023, 8, 3 ,10,00),
+
+}
+def get_data():
+    import requests
+    data = requests.get('https://randomuser.me/api/')
+    print(data.json())
+
+def stream_data():
+    import json
+    
+'''with DAG('user_automation',default_args=default_args,
+         schedule_interval='@daily',catchup=False) as dag:
+    
+    streaming_task = PythonOperator(
+        task_id='streaming_data_from_api',
+        python_callable=stream_data,
+        )'''
+stream_data()
+
